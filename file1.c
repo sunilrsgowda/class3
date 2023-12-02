1d68abfa33b1a6bb374a9994d1f4228e1901560d
sunilrsgowda <sunil.kumar@gndsolutions.in>
2023-12-02 14:26:42 +0530

    added file2

--- file1.c
+++ file1.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main()
 {
+    int a,b,c;
+
+    printf("enter a b c values");
+    scanf("%d %d %d",&a,&b,&c);
+    printf("a b and c values are\",a,b,c);
+
     printf("hellow word");
+
 }
---
+++ file2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int a=90,b=78;
+    printf("a and b values are",a,b);
+    return 0;
+}
